b561c201f7c27bf5aa28d86877fd2306d8485ba1
Hossein <hossein82shakeri@gmail.com>
2022-08-23 13:30:46 +0430

    Create Statistics.py

---
+++ Statistics.py
@@ -0,0 +1,28 @@
+def totalGrowth(list):
+    index = 0
+    memory = []
+    growth = []
+    for item in list:
+        if index == 0 :
+            memory.append(item)
+            index += 1
+            continue
+        memory.append(item)
+        growth.append(memory[-1] - memory[-2])
+        
+    return sum(growth)
+
+def partialGrowth(list):
+    try:
+        if list[-1] != 0:
+            result = (list[-1] / list[-2])*100
+        else :
+            result = (list[-2] * 100)
+            return - result
+    except ZeroDivisionError:
+        if list[-1] != 0:
+            result = (list[-1] *100)
+            return result
+        else:
+            result = 100
+    return int(result) - 100
